6643f227b207d47f215311884bdc6b29e10f8278
michm <puppetpup@protonmail.com>
2021-09-10 15:30:46 +0100

    start

---
+++ src/test/java/com/example/mvc/demo/mvc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvc.demo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
