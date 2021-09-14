2024b127ecc755335b405f8be5e216e813d5e8de
Michele <m.montecchiani@betacom.it>
2021-09-14 10:45:58 +0200

    gestione pol semifinito

--- src/main/java/com/spritzleaf/puppet/mvc/controllers/GestisciPOLController.java
+++ src/main/java/com/spritzleaf/puppet/mvc/controllers/GestisciPOLController.java
@@ -1,10 +1,25 @@
 package com.spritzleaf.puppet.mvc.controllers;
 
 
+import com.spritzleaf.puppet.mvc.constants.WebUrls;
+import com.spritzleaf.puppet.mvc.service.POLService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GestisciPOLController {
+
+    @Autowired
+    private POLService polService;
+
+    @GetMapping(WebUrls.GEST_POL)
+    public String getPols(Model model) {
+        model.addAttribute("pols", polService.getAllPOLs());
+        model.addAttribute("title", "Gestisci POL");
+        return "gestisciPol";
+    }
 
 
 }
